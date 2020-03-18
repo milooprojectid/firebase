535257d75f8fbf46bc4518150752e9dc21d4dbcd
Archie <archie.isdiningrat@gmail.com>
2020-03-18 15:04:28 +0700

    update

--- functions/src/handlers/covid_notification_trigger.ts
+++ functions/src/handlers/covid_notification_trigger.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import axios from 'axios';
+
 import { determineFirestoreAction } from '../utils/helpers';
 import { ACTION } from '../utils/constant';
 
@@ -13,10 +15,9 @@ interface Covid19Data {
 export const covid19FirestoreOnChange = functions.region('asia-east2').firestore.document(covidDataPath)
     .onWrite(async (change, context) => {
         try {
-            const { action, data } = determineFirestoreAction<Covid19Data>(change);
-            if (action === ACTION.UPDATE){
-                console.log('data changed');
-                console.table(data);
+            const { action, data, dataBefore } = determineFirestoreAction<Covid19Data>(change);
+            if ((action === ACTION.UPDATE) && (data.confirmed != dataBefore?.confirmed)){
+                await axios.post(process.env.SERVICE_BOT + '/subscription/notify');
             } 
         } catch (err) {
             console.error(err.message);
