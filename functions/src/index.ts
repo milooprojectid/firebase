96387739ed3fbe3b2f5a9a17780d91d20b0eb93e
Archie <archie.isdiningrat@gmail.com>
2020-03-18 14:26:01 +0700

    init app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,10 @@
 import * as BuildNotification from './handlers/build_notification';
+import * as admin from 'firebase-admin';
 import { loadEnv } from './utils/load_env';
 
+/** initialize */
 loadEnv();
+admin.initializeApp();
 
 export = {
     ...BuildNotification
